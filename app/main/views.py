7434605c8b52b7cbc020a95e2d96680ca6005f12
Kasparov <ianjkiplagat@gmail.com>
2021-04-23 14:33:45 +0300

    add registration form

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,request,redirect,url_for
+from flask_login import login_required
 from . import main
 
 @main.route('/')
@@ -11,3 +12,19 @@ def index():
     title = 'Home'
     
     return render_template('index.html', title = title)
+
+
+# @main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+# @login_required
+# def comment(pitch_id):
+#     form = CommentForm()
+#     pitch = Pitch.query.get(pitch_id)
+#     all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+#     if form.validate_on_submit():
+#         comment = form.comment.data 
+#         pitch_id = pitch_id
+#         user_id = current_user._get_current_object().id
+#         new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+#         new_comment.save_c()
+#         return redirect(url_for('.comment', pitch_id = pitch_id))
+#     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
