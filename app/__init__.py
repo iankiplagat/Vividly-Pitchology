135ae3276610a7322d82c54be20d40f2501b4498
Kasparov <ianjkiplagat@gmail.com>
2021-04-23 10:40:01 +0300

    add 404 page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
@@ -12,3 +13,9 @@ def create_app(config_name):
    
     # Initializing flask extensions
     bootstrap.init_app(app)    
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
